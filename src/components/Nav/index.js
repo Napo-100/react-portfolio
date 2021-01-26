fcc85333bc996d4eb0ba7a79bd9aef61ba6814bd
Napo <Michaeljnapolitano@gmail.com>
2021-01-26 18:39:47 -0500

    updated resume

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,11 +3,10 @@ import React from "react";
 import logo from "../../logo.svg";
 import "../../App.css";
 import "../../index.css";
-import resume from "../../assets/files/Michael Napolitano-dev-resume.pdf";
-
+import resume from "../../assets/files/Michael-Napolitano-dev-resume-for-portfolio.pdf";
 
 function Nav(props) {
-  const tabs =  ['About Me', 'Projects', 'Contact'];
+  const tabs = ["About Me", "Projects", "Contact"];
   return (
     <nav className="bg-black flex justify-center lg:justify-between flex-wrap p-3 md:p-6 w-full h-36">
       <div className="flex items-center text-white mr-0">
@@ -16,27 +15,30 @@ function Nav(props) {
           src={logo}
           alt="react logo"
         />
-        <button className="focus:outline-none"
-        onClick={() => props.handlePageChange("#")}>
+        <button
+          className="focus:outline-none"
+          onClick={() => props.handlePageChange("#")}
+        >
           <h1 className="text-4xl flex justify-center px-2">
             Michael Napolitano
           </h1>
         </button>
       </div>
-      
+
       <div
         className="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block pt-4 sm:pt-6 lg:pt-0"
         id="nav-content"
       >
         <ul className="list-reset flex justify-center lg:justify-end flex-1 items-center text-xs xl:text-2xl sm:text-lg sm:font-medium">
-          {tabs.map(tab => (
-          <a key={tab}
-            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md  ring-2 mr-4 focus:bg-blue-500 focus:text-white"
-            href={'#' + tab}
-            onClick={() => props.handlePageChange(tab)}
-          >
-            {tab}
-          </a>
+          {tabs.map((tab) => (
+            <a
+              key={tab}
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md  ring-2 mr-4 focus:bg-blue-500 focus:text-white"
+              href={"#" + tab}
+              onClick={() => props.handlePageChange(tab)}
+            >
+              {tab}
+            </a>
           ))}
           <a
             className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md ring-2 focus:bg-blue-500 focus:text-white"
